39222d441d07195504a28034a21868428103c859
Po-Ying Chen <poying.me@gmail.com>
2014-12-22 17:12:01 +0800

    tools

--- tools/start.sh
+++ tools/start.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
 
-NSQLOOKUP_CID=$(docker run --name iv_nsqlookupd -p 4160:4160 -p 4161:4161 -d nsqio/nsqlookupd);
+NSQLOOKUP_CID=$(docker run --name ${YMIS_CONTAINER_NAME_PREFIX}nsqlookupd -p 4160:4160 -p 4161:4161 -d nsqio/nsqlookupd);
 NSQLOOKUP_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${NSQLOOKUP_CID})
 echo $NSQLOOKUP_CID
 
 docker run \
-  --name iv_nsqd \
-  --link iv_nsqlookupd:nsqlookupd \
+  --name ${YMIS_CONTAINER_NAME_PREFIX}nsqd \
+  --link ${YMIS_CONTAINER_NAME_PREFIX}nsqlookupd:nsqlookupd \
   -d nsqio/nsqd --lookupd-tcp-address=${NSQLOOKUP_ADDRESS}:4160
 
 docker run \
-  --name iv_nsqadmin \
-  --link iv_nsqlookupd:nsqlookupd \
+  --name ${YMIS_CONTAINER_NAME_PREFIX}nsqadmin \
+  --link ${YMIS_CONTAINER_NAME_PREFIX}nsqlookupd:nsqlookupd \
   -p 4171:4171 \
   -d nsqio/nsqadmin --lookupd-http-address=${NSQLOOKUP_ADDRESS}:4161
 
 docker run \
-  --name iv_app \
-  --link iv_nsqd:nsqd \
-  --link iv_nsqlookupd:nsqlookupd \
+  --name ${YMIS_CONTAINER_NAME_PREFIX}app \
+  --link ${YMIS_CONTAINER_NAME_PREFIX}nsqd:nsqd \
+  --link ${YMIS_CONTAINER_NAME_PREFIX}nsqlookupd:nsqlookupd \
   -v $(pwd)/app:/app \
   -p 8080:80 \
-  youmeb/invoice-service make
+  ${YMIS_IMAGE} make
--- tools/stop.sh
+++ tools/stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker stop $(docker ps -a -q --filter="name=iv_")
-docker rm $(docker ps -a -q --filter="name=iv_")
+docker stop $(docker ps -a -q --filter="name=${YMIS_CONTAINER_NAME_PREFIX}")
+docker rm $(docker ps -a -q --filter="name=${YMIS_CONTAINER_NAME_PREFIX}")
