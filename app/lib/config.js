fa8c2a1c19c0afb5ec1d30b98da64b9d444b0b41
Po-Ying Chen <poying.me@gmail.com>
2014-12-22 17:11:46 +0800

    Update lib/config.js

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,3 +1,18 @@
 'use strict';
 
+var path = require('path');
+var assign = require('object-assign');
+var flags = require('./flags');
+var config = require('../config.default');
+var cwd = process.cwd();
 
+// 如果使用者有指定 config 檔案，那需要把他跟預設設定合併
+if (flags.config) {
+  let tmp = require(flags.config);
+  assign(config, tmp);
+}
+
+// 路徑
+config.logdir = path.resolve(config.logdir);
+
+module.exports = config;
