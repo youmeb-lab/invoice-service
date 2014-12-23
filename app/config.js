cc27f23bc77c14ed0c0a4ceeefac923486d57d8f
Po-Ying Chen <poying.me@gmail.com>
2014-12-23 14:33:13 +0800

    default config

--- app/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var config = module.exports;
-
-config.cetustek = {
-  endpoint: 'http://invoice.cetustek.com.tw/InvoiceB2C/InvoiceAPI?wsdl',
-  user: '53935964',
-  password: '53935964tnca'
-};
